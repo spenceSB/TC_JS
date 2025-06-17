8ab46aef12adab956e07be9e60ca12ca8f514b02
spenceSB <spencer.christian@smartbear.com>
2025-06-17 12:03:28 -0400

    Group Execution Plan

---
+++ JS_Suite/Script/EditXML.js
@@ -0,0 +1,44 @@
+﻿const XML_PATH = "C:\github\XML\Data.xml"
+  var xmlDoc = Sys.OleObject("Msxml2.DOMDocument.6.0");
+function TestWithXPath()
+{
+  var Doc, s, Nodes, ChildNodes, i, Node;
+  
+  // Create a COM object
+  // If you have MSXML 4:
+//  Doc = getActiveXObject("Msxml2.DOMDocument.4.0");
+  // If you have MSXML 6:
+//  Doc = getActiveXObject("Msxml2.DOMDocument.6.0");
+
+//  Doc.async = false;
+  
+  // Load data from a file
+  // We use the file created earlier
+  Doc.load("C:\\Data.xml");
+  
+  // Report an error, if, for instance, the markup or file structure is invalid
+  if(Doc.parseError.errorCode != 0)
+  {
+    s = "Reason:\t" + Doc.parseError.reason + "\n" +
+        "Line:\t" + aqConvert.VarToStr(Doc.parseError.line) + "\n" +
+        "Pos:\t" + aqConvert.VarToStr(Doc.parseError.linePos) + "\n" +
+        "Source:\t" + Doc.parseError.srcText;
+    // Post an error to the log and exit
+    Log.Error("Cannot parse the document.", s);
+    return;
+  }
+  
+  // Use an XPath expression to obtain a list of "control" nodes
+  Nodes = Doc.selectNodes("//control");
+  
+  // Process the node
+  for(i = 0; i < Nodes.length; i++)
+  {
+    // Get the node from the collection of the found nodes
+    Node = Nodes.item(i);
+    // Get child nodes
+    ChildNodes = Node.childNodes;
+    // Output two child nodes to the log
+    Log.Message(ChildNodes.item(1).text + ": " + ChildNodes.item(2).text);
+  }
+}
---
+++ JS_Suite/Script/LogMessageTest.js
@@ -0,0 +1,30 @@
+﻿//USEUNIT EditXML
+
+function logMessage(){
+  Log.Message("Passing Test")
+
+  Log.SaveResultsAs("C:\\TestResults\\Log\\LogResults2.xml", lsJUnit)
+    xmlDoc.async = false;
+  xmlDoc.load(xmlFilePath);
+
+}
+function LogPropertyConfig(){
+  AddXrayPropertyToJUnitXML('C:\\TestResults\\Log\\LogResults2.xml', 'ANLIT-5986')
+}
+
+function AddXrayPropertyToJUnitXML(xmlFilePath, testKey) {
+//  var xmlDoc = Sys.OleObject("Msxml2.DOMDocument.6.0");
+  xmlDoc.async = false;
+  xmlDoc.load(xmlFilePath);
+
+  var testcases = xmlDoc.getElementsByTagName("testcase");
+  for (var i = 0; i < testcases.length; i++) {
+    var properties = xmlDoc.createElement("properties");
+    var property = xmlDoc.createElement("property");
+    property.setAttribute("name", "test_key");
+    property.setAttribute("value", testKey); // You can set this dynamically per test case
+    properties.appendChild(property);
+    testcases.item(i).appendChild(properties);
+  }
+  xmlDoc.save(xmlFilePath);
+}
